f734a90301da5aae1b09387c6ddc5df5bbe35b14
by8lab <by8tech@gmail.com>
2026-05-20 17:12:37 +0530

    updated

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -29,6 +29,8 @@ class Settings(BaseSettings):
         "http://localhost:8080",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
+        "https://audit-project-blond.vercel.app",
+        "*"
     ]
 
     # Mock Data
